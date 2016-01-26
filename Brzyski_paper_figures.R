0da996c8ba29414d93175258476b54e083077490
agisga <alexej.go@googlemail.com>
2016-01-26 00:15:21 -0600

    code for figures 3(a) and (b) in Brzyski et. al.

--- Brzyski_paper_figures.R
+++ Brzyski_paper_figures.R
@@ -13,7 +13,7 @@ library(grpSLOPE)
 
 # Adjust the number of cores to the particular system
 library(doParallel)
-registerDoParallel(cores=10)
+registerDoParallel(cores=2)
 
 ####################################
 # Figure 1
@@ -131,3 +131,133 @@ plot(n.relevant, pow, ylim=c(0,1), type="b",
 segments(n.relevant, pow-pow.sd, n.relevant, pow+pow.sd, col="grey")
 segments(n.relevant-1, pow-pow.sd, n.relevant+1, pow-pow.sd, col="grey")
 segments(n.relevant-1, pow+pow.sd, n.relevant+1, pow+pow.sd, col="grey")
+
+
+####################################
+# Figure 3
+####################################
+
+fdr <- 0.1
+n.iter <- 200
+
+p <- 5000
+n.group <- 1000
+group <- rep(1:1000, each=5)
+group.id <- getGroupID(group)
+group.length <- sapply(group.id, FUN=length)
+wt <- rep(sqrt(5), p)
+B <- sqrt(4*log(n.group) * (1 - n.group^(-2/5)) - 5)
+n.relevant <- floor(seq(1, 100, length=10))
+
+# generate lambda
+lambda.max <- lambdaGroupSLOPE(fdr=fdr, group=group,
+                               wt=sqrt(group.length),
+                               method="chiOrthoMax")
+lambda.chi <- lambdaGroupSLOPE(fdr=fdr, n.obs=p, group=group, 
+                               wt=sqrt(group.length),
+                               method="chiEqual")
+
+FDR.chi    <- rep(NA, length(n.relevant))
+FDR.chi.sd <- rep(NA, length(n.relevant))
+FDR.max    <- rep(NA, length(n.relevant))
+FDR.max.sd <- rep(NA, length(n.relevant))
+
+one.iteration <- function(n.signif){
+  # model matrix
+  X <- matrix(rnorm(p^2, sd=sqrt(1/p)), p, p)
+
+  # generate coeffient vector, pick relevant groups at random
+  b <- rep(0, p)
+  ind.relevant <- sample(1:n.group, n.signif)
+  for (j in ind.relevant) {
+    X1 <- apply(X[ , group.id[[j]]], 1, sum)
+    b[group.id[[j]]] <- (B / norm(as.matrix(X1), "f")) * rep(1, group.length[j])
+  }
+
+  # generate the response vector
+  y <- X %*% b + rnorm(p, sd=1)
+
+  # get Group SLOPE solution
+  b.grpSLOPE.chi <- proximalGradientSolverGroupSLOPE(y=y, A=X, group=group,
+                                                     wt=wt, lambda=lambda.chi,
+                                                     verbose=FALSE)
+  b.grpSLOPE.max <- proximalGradientSolverGroupSLOPE(y=y, A=X, group=group,
+                                                     wt=wt, lambda=lambda.max,
+                                                     verbose=FALSE)
+
+  # FDR and power
+  nonzero <- rep(NA, n.group)
+  for (j in 1:n.group) { nonzero[j] <- (sum(b.grpSLOPE.chi$x[group.id[[j]]]^2) > 0) }
+  truepos <- sum(nonzero[ind.relevant])
+  falsepos <- sum(nonzero) - truepos
+  FDR.chi <- falsepos / max(1, sum(nonzero))
+
+  nonzero <- rep(NA, n.group)
+  for (j in 1:n.group) { nonzero[j] <- (sum(b.grpSLOPE.max$x[group.id[[j]]]^2) > 0) }
+  truepos <- sum(nonzero[ind.relevant])
+  falsepos <- sum(nonzero) - truepos
+  FDR.max <- falsepos / max(1, sum(nonzero))
+
+  return(list(FDR.chi=FDR.chi, FDR.max=FDR.max))
+}
+
+for (k in 1:length(n.relevant)) {
+  parallel.results <- foreach(i=1:n.iter) %dopar% {
+    one.iteration(n.relevant[k])
+  }
+
+  FDR.chi.vec <- rep(NA, n.iter)
+  FDR.max.vec <- rep(NA, n.iter)
+
+  for (j in 1:n.iter) {
+    FDR.chi.vec[j] <- parallel.results[[j]]$FDR.chi
+    FDR.max.vec[j] <- parallel.results[[j]]$FDR.max
+  }
+
+  FDR.chi[k]    <- mean(FDR.chi.vec)
+  FDR.chi.sd[k] <- sd(FDR.chi.vec)
+  FDR.max[k]    <- mean(FDR.max.vec)
+  FDR.max.sd[k] <- sd(FDR.max.vec)
+  
+  print(paste(k, "sparsity levels completed"))
+}
+
+#####################################################
+# Figure 3 (a) - q=0.1, Brzyski et. al. (2015)
+#####################################################
+
+# plot FDR -------------------------
+plot(n.relevant, FDR.max, ylim=c(0,0.3), type="b",
+     xlab="Number of relevant groups", ylab="Estimated gFDR", 
+     main="gFDR for lambda_max")
+
+# FDR nominal level
+abline(fdr, 0)
+
+# FDR error bars
+segments(n.relevant, FDR.max-FDR.max.sd, n.relevant,
+         FDR.max+FDR.max.sd, col="grey")
+segments(n.relevant-1, FDR.max-FDR.max.sd, n.relevant+1,
+         FDR.max-FDR.max.sd, col="grey")
+segments(n.relevant-1, FDR.max+FDR.max.sd, n.relevant+1,
+         FDR.max+FDR.max.sd, col="grey")
+
+#############################################################
+# Figure 3 (b) - q=0.1, Brzyski et. al. (2015)
+#############################################################
+
+# plot FDR -------------------------
+plot(n.relevant, FDR.chi, ylim=c(0,0.3), type="b",
+     xlab="Number of relevant groups", ylab="Estimated gFDR", 
+     main="gFDR for corrected lambda")
+
+# FDR nominal level
+abline(fdr, 0)
+
+# FDR error bars
+segments(n.relevant, FDR.chi-FDR.chi.sd, n.relevant,
+         FDR.chi+FDR.chi.sd, col="grey")
+segments(n.relevant-1, FDR.chi-FDR.chi.sd, n.relevant+1,
+         FDR.chi-FDR.chi.sd, col="grey")
+segments(n.relevant-1, FDR.chi+FDR.chi.sd, n.relevant+1,
+         FDR.chi+FDR.chi.sd, col="grey")
