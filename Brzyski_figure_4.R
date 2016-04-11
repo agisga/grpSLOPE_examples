e6114c1ad24b1df2094faea122338ba9993ac629
agisga <alexej.go@googlemail.com>
2016-04-11 15:00:30 -0500

    code for Brzyski et. al. figure 4 no also saves the sigma estimates

--- Brzyski_figure_4.R
+++ Brzyski_figure_4.R
@@ -42,10 +42,12 @@ signal.strength <- sum(Bfun(group.length)) / n.group
 
 n.relevant <- floor(seq(1, 60, length=7))
 
-FDR    <- rep(NA, length(n.relevant))
-FDR.sd <- rep(NA, length(n.relevant))
-pow    <- rep(NA, length(n.relevant))
-pow.sd <- rep(NA, length(n.relevant))
+FDR      <- rep(NA, length(n.relevant))
+FDR.sd   <- rep(NA, length(n.relevant))
+pow      <- rep(NA, length(n.relevant))
+pow.sd   <- rep(NA, length(n.relevant))
+sigma    <- rep(NA, length(n.relevant))
+sigma.sd <- rep(NA, length(n.relevant))
 
 one.iteration <- function(n.signif){
   # generate and normalize the model matrix
@@ -79,7 +81,7 @@ one.iteration <- function(n.signif){
   FDR <- n.falsepos / max(1, n.selected)
   pow <- n.truepos / length(true.relevant)
 
-  return(list(FDR=FDR, pow=pow))
+  return(list(FDR=FDR, pow=pow, sigma=b.grpSLOPE$sigma))
 }
 
 for (k in 1:length(n.relevant)) {
@@ -87,18 +89,22 @@ for (k in 1:length(n.relevant)) {
     one.iteration(n.relevant[k])
   }
 
-  FDR.vec  <- rep(NA, n.iter)
-  pow.vec  <- rep(NA, n.iter)
+  FDR.vec   <- rep(NA, n.iter)
+  pow.vec   <- rep(NA, n.iter)
+  sigma.vec <- rep(NA, n.iter)
 
   for (j in 1:n.iter) {
-    FDR.vec[j]  <- parallel.results[[j]]$FDR
-    pow.vec[j]  <- parallel.results[[j]]$pow
+    FDR.vec[j]   <- parallel.results[[j]]$FDR
+    pow.vec[j]   <- parallel.results[[j]]$pow
+    sigma.vec[j] <- parallel.results[[j]]$sigma
   }
 
   FDR[k] <- mean(FDR.vec)
   FDR.sd[k] <- sd(FDR.vec)
   pow[k] <- mean(pow.vec)
   pow.sd[k] <- sd(pow.vec)
+  sigma[k] <- mean(sigma.vec)
+  sigma.sd[k] <- sd(sigma.vec)
   
   print(paste(k, "sparsity levels completed"))
 }
